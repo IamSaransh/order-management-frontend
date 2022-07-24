c210f51bbe36ca29bbf771e7d6f2ace899a18cc7
saransh <saranshvashistha@gmail.com>
2022-07-24 12:38:58 +0530

    feat: login service and undepreciated subscribe added

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -3,22 +3,25 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  private host: string = environment.apiUrl;
-  private token: string;
-  private loggedInUsername: string;
+  public host: string = environment.apiUrl;
+  private token: string = "";
+  private loggedInUsername: string = "";
+  private jwtHelper = new JwtHelperService();
 
   constructor( private http: HttpClient) { 
   }
 
-  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse>{
-    return this.http.post<HttpResponse <any> | HttpErrorResponse>
-      (`${this.host}/authorization-service/auth/v1/login`, user, {observe: 'response'});
+  public login(user: User): Observable<HttpResponse<User> >{
+    return this.http.post<User>  (`${this.host}/auth/v1/login`, user, {observe: 'response'});
+      // (`${this.host}/authorization-service/auth/v1/login`, user, {observe: 'response'});
+     
   }
 
   public register(user: User): Observable<User | HttpErrorResponse>{
@@ -29,12 +32,29 @@ export class AuthenticationService {
   //TODO: add method and model for this as well!
   public authenticate(user: User): Observable<HttpResponse<any> | HttpErrorResponse>{
     return this.http.post<User | HttpErrorResponse>
-      (`${this.host}/authorization-service/auth/v1/register`, user, {observe: 'response'});
+      (`${this.host}/authorization-service/auth/v1/authenticate`, user, {observe: 'response'});
+  }
+
+  public isLoggedIn(): boolean{
+    this.loadToken();
+    if(this.token!=null && this.token !==''){
+      if(this.jwtHelper.decodeToken(this.token).sub!=null || ''){
+        if(!this.jwtHelper.isTokenExpired(this.token)){
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          return true;
+        }
+      }
+    }
+    else{
+      this.logout();
+      return false;
+    }
+    return false;
   }
 
   public logout(): void{
-    this.token=null;
-    this.loggedInUsername=null;
+    this.token="";
+    this.loggedInUsername="";
     localStorage.removeItem('user');
     localStorage.removeItem('token');
   }
@@ -44,12 +64,20 @@ export class AuthenticationService {
     localStorage.setItem('token', token);
   }
 
+  public loadToken(): void{
+    this.token = localStorage.getItem('token') || "";
+  }
+
+  public getToken(): string{
+    return this.token;
+  }
+
   public addUserToLocalCache(user: User): void{
     localStorage.setItem('user', JSON.stringify(user));
   }
 
   public getUserFromLocalCache(): User{
-    return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse( localStorage.getItem('user') || '{}' );
   }
   
 }
